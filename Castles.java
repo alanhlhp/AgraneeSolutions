c43c94c4422dc23d0e870cf1ee2b998f0a477d27
Alan H. Zacarias <alanh.lhp@gmail.com>
2018-03-29 11:09:41 -0600

    Update Castles.java
    Change from HashSet<Integer> to ArrayList<ArrayList<Integer>>

--- Castles.java
+++ Castles.java
@@ -10,21 +10,48 @@ class Castles{
 	}
 
 	public int countCastles(int[] a){
-		HashSet<Integer> peaks = new HashSet<>();
-		HashSet<Integer> valleys = new HashSet<>();
+		ArrayList<ArrayList<Integer>> peaks = new ArrayList<>();
+		ArrayList<ArrayList<Integer>> valleys = new ArrayList<>();
+		ArrayList<Integer> sub = new ArrayList<>();
+		boolean same = false;
 		int ref = 0;
 
 		for(int i=1; i<a.length-1; i++){
-			
-			if( (a[i] >= a[i-1]) && (a[i] >= a[i+1]) && a[i]>=ref ){
-				peaks.add(a[i]);
-				ref = a[i-1];
-			} else if ( (a[i] <= a[i-1]) && (a[i] <= a[i+1]) ){
-				valleys.add(a[i]);
-				ref = a[i-1];
-			}
 
+			if(same){
+				sub.add(a[i]);
+				if(a[i]!=a[i+1]){
+					same = false;
+					if(a[i]>a[i+1]){
+						peaks.add(sub);
+					} else{
+						valleys.add(sub);
+					}
+				}
+			} else{
+				sub = new ArrayList<>();
+
+				if( (a[i] >= a[i-1]) && (a[i] >= a[i+1]) ){
+					sub.add(a[i]);
+					if(a[i]==a[i+1]){
+						same = true;
+					} else{
+						peaks.add(sub);
+					}
+
+				} else if ( (a[i] <= a[i-1]) && (a[i] <= a[i+1]) ){
+					sub.add(a[i]);
+					if(a[i]==a[i+1]){
+						same = true;
+					} else{
+						valleys.add(sub);
+					}
+				}
+			}
 		}
+
+		//System.out.println("Peaks: " + peaks);
+		//System.out.println("Valleys" + valleys);
 
 		return peaks.size()+valleys.size();
 	}
